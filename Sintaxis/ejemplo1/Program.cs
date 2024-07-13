362181b72ce49bb1f927a7566a8242a5c25590ea
unknown <poncefernandoandres@gmail.com>
2024-07-13 20:20:40 -0300

    Agregamos

---
+++ Sintaxis/ejemplo1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Hello");
+            Console.ReadKey();
+            
+        }
+    }
+}
